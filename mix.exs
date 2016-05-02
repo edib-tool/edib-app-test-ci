7f1b29ad1cc3010a4a5f00ef71ecd6f5fdba84d6
Christoph Grabo <asaaki@mannaz.cc>
2016-05-02 14:49:32 +0200

    Add more deps

--- mix.exs
+++ mix.exs
@@ -4,21 +4,31 @@ defmodule EdipAppTestCi.Mixfile do
   def project do
     [app: :edib_app_test_ci,
      version: "0.1.0",
-     elixir: "~> 1.0",
+     elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
   def application do
-    [applications: [:logger, :cmark, :fnv]]
+    [applications: [
+      :logger,
+      :cmark, :fnv,
+      :ralitobu, :ralitobu_plug,
+      :strinx,
+      :flasked,
+    ]]
   end
 
   defp deps do
     [
-      {:exrm, "~> 0.19"},
-      {:cmark, "~> 0.5"},
-      {:fnv, "~> 0.2"},
+      {:exrm, "~> 1.0"},
+      {:cmark, "~> 0.6"},
+      {:fnv, "~> 0.3"},
+      {:ralitobu, "~> 0.1"},
+      {:ralitobu_plug, "~> 0.1"},
+      {:flasked, "~> 0.3"},
+      {:strinx, "~> 0.2"},
     ]
   end
 end
